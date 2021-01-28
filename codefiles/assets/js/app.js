5734f09da4cf174b32e72b67780a243dab9c1757
hekutoru-dev <hectorh.padilla@gmail.com>
2021-01-28 09:34:48 -0600

    Load data into the project

---
+++ codefiles/assets/js/app.js
@@ -0,0 +1,14 @@
+
+// Set margin for the layout
+let margin = {
+    top: 20,
+    right: 40,
+    botton: 110,
+    left: 100
+}
+
+// Import csv data.
+d3.csv("assets/data/data.csv").then((data) => {
+    console.log(data);
+
+});
