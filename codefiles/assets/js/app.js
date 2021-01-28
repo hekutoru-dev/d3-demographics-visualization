fa12512e4b58ef359b0183a036d4451d9782affb
hekutoru-dev <hectorh.padilla@gmail.com>
2021-01-28 09:36:38 -0600

    Set chart and svg dimensions

--- codefiles/assets/js/app.js
+++ codefiles/assets/js/app.js
@@ -7,6 +7,20 @@ let margin = {
     left: 100
 }
 
+// Define svg area dimensions
+let svgWidth = 960, svgHeight = 550;
+let chartWidth = svgWidth - margin.left - margin.right;
+let chartHeight = svgHeight - margin.top - margin.bottom;
+
+// Create SVG wrapper and group.
+let svg = d3.select('#scatter')
+            .append('svg')
+            .attr('height', svgHeight)
+            .attr('width', svgWidth);
+
+let chartGrp = svg.append('g')
+            .attr('transform', `translate(${margin.left}, ${margin.top})`);          
+
 // Import csv data.
 d3.csv("assets/data/data.csv").then((data) => {
     console.log(data);
